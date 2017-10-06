fc1c63bb82d5a94bc0adc82e3f883332aad6e7a3
Luis Felipe Ferreira <luislipe08@gmail.com>
2017-10-06 15:18:05 -0300

    AV1
    Unity Editor

---
+++ Assets/Editor/EditorCustom.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MudarValor))]
+public class EditorCustom : Editor
+{
+    int value;
+    int index;
+   
+    public override void OnInspectorGUI()
+    {
+        MudarValor MudarValor = target as MudarValor;
+        
+        value = EditorGUILayout.IntField("Valor", value);
+        if (GUILayout.Button("Acrescentar"))
+        {
+            MudarValor.Add(MudarValor.List.Count.ToString(), value);
+        }
+        EditorGUI.EndDisabledGroup();
+        EditorGUILayout.LabelField("Remover", EditorStyles.boldLabel);
+        index = EditorGUILayout.IntField("Valor", index);
+        if (GUILayout.Button("Remover"))
+        {
+            MudarValor.Remove(index);
+        }
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.LabelField("Lista", EditorStyles.boldLabel);
+        
+        for (int i = 0; i < MudarValor.List.Count; i++)
+        {
+            EditorGUILayout.FloatField(MudarValor.List[i].key, MudarValor.List[i].value);
+        }  
+        EditorGUILayout.LabelField("Acrescentar", EditorStyles.boldLabel);
+       
+        //MC Lessinha
+        
+    }
+}
---
+++ Assets/Scripts/MudarValor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudarValor : MonoBehaviour
+{  
+    public List<Entrada> List  = new List<Entrada>(0);
+
+   
+    public void Add(string key, int value)
+    {
+        Entrada entrada = new Entrada();
+        entrada.key = key;
+        entrada.value = value;
+        List.Add(entrada);
+    }
+
+
+
+
+
+
+
+    public void Remove(int index)
+    {
+        List.RemoveAt(index);
+    }
+   
+}
+public class Entrada
+{
+    public string key;
+    public int value;
+}
+
+
